8b7698869d780c03cee1397a40521e03ca609179
Mozhgan Moqadam <mozhganmogadam@gmial.com>
2023-11-08 15:35:57 +0100

    recipe menu, header ,recipe list pages

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,15 @@
+export class recipe{
+public name:string;
+public description:string;
+public imagepath:string;
+
+constructor(name:string,desc:string,imagepath:string){
+    this .imagepath=imagepath;
+    this.description=desc;
+    this .name=name
+}
+
+
+
+
+}
