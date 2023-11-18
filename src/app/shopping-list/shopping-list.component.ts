d053ba70d3f17a2b86d7017a65f9c7d963381027
Mozhgan Moqadam <mozhganmogadam@gmial.com>
2023-11-18 11:52:26 +0100

    Shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../share/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
-
+export class ShoppingListComponent implements OnInit {
+  ngOnInit(): void {}
+ 
+  Ingredients:Ingredient[]=[
+    new Ingredient('Apple',5),
+    new Ingredient('Tomato',2),
+    new Ingredient('Pasta',3),
+    new Ingredient('Mashrom',12),
+    new Ingredient('Salami',4),
+    new Ingredient('Cheese',1),
+  ];
 }
