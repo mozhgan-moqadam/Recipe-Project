48aaa50971e70375fe294546b1b2cf0963f6f42c
Mozhgan Moqadam <mozhganmogadam@gmial.com>
2023-11-18 11:51:35 +0100

    Recipe Detail

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/share/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
-export class RecipeDetailComponent {
+export class RecipeDetailComponent implements OnInit {
+  ngOnInit(): void {}
 
+ 
 }
