327f6a3b63d3151c8ae40a381a2d62b2b3a2381e
Mozhgan Moqadam <mozhganmogadam@gmial.com>
2023-12-05 18:36:08 +0100

    navigation recipe and shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Recipe-Project';
-  
+  //  selectedSection=true;
+   loadedFeature='recipe';
+   
+  //  onNavigate(feature: string){
+  //   this.featureSelected = feature;
+  //  }
+
+   onNavigate(feature:string){
+    this.loadedFeature=feature
+   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
-    selector:'app-header',
-    templateUrl:'\header.component.html',
+    selector: 'app-header',
+    templateUrl: '\header.component.html',
 })
-export class headerComponent{
+export class headerComponent {
+    @Output() featureSelected = new EventEmitter<string>();
 
+    // onSelect(section: boolean) {
+    //     this.featureSelected.emit(section);
+    // }
+
+    onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
 }
