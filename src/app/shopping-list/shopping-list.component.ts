7256651ce769f52fec604e9cfe160fdef64670db
Mozhgan Moqadam <mozhganmogadam@gmial.com>
2023-12-11 15:54:13 +0100

    click to add new shopping items

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,14 +6,23 @@ import { Ingredient } from '../share/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ngOnInit(): void {}
- 
-  Ingredients:Ingredient[]=[
-    new Ingredient('Apple',5),
-    new Ingredient('Tomato',2),
-    new Ingredient('Pasta',3),
-    new Ingredient('Mashrom',12),
-    new Ingredient('Salami',4),
-    new Ingredient('Cheese',1),
+  ngOnInit(): void { }
+
+  Ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomato', 2),
+    new Ingredient('Pasta', 3),
+    new Ingredient('Mashrom', 12),
+    new Ingredient('Salami', 4),
+    new Ingredient('Cheese', 1),
   ];
+
+  // onIngredientAdded(ingredient = Ingredient) {
+  //   this.ingredients.push(ingredient);
+  // }
+  onIngredientAdded(ingredient: Ingredient) {
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+    this.Ingredients.push(newIngredient);
+  }
+  
 }
